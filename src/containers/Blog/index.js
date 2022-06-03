fefced34e66baf2956afa70b23cdfe3c193a5ac6
tzd <582361831@qq.com>
2022-06-03 23:18:27 +0800

    fix

---
+++ src/containers/Blog/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Tian Zhengdong
+ * @Date: 2022-06-03 23:17:07
+ * @Description: 
+ */
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return <div>123</div>;
+};

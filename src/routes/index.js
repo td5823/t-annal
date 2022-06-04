8831e331da381574503a3842955197181a738e8e
tzd <582361831@qq.com>
2022-06-04 22:13:49 +0800

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // 登录模块
 const Login = React.lazy(() => import("../containers/Login"));
-// const Login =import("../containers/Login");
-// import Login from "../containers/Login";
 
 const routers = [
   {
@@ -19,6 +17,7 @@ const tRouter = () => {
     <Router>
       <Routes>
         {routers.map((item, index) => {
+          const { ...other } = item;
           return (
             <Route
               caseSensitive // 区分大小写
@@ -26,6 +25,7 @@ const tRouter = () => {
               exact
               path={item.path}
               element={<item.component />} // 不是老版本的：component 或 render
+              {...other}
             />
           );
         })}

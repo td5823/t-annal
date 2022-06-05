bbc38bc661e2608bb6a3d1a28216c48ac02ab7a9
tzd <582361831@qq.com>
2022-06-05 11:47:26 +0800

    集成axios

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -3,14 +3,40 @@
  * @Date: 2022-06-03 23:17:07
  * @Description:
  */
-import React from "react";
-import { Button, Space } from "antd";
+import React, { useRef } from "react";
+import { Button, Space, Row, Col, Input, Form } from "antd";
 
 const Login = () => {
+  const formRef = useRef();
+
+  const handleLogin = () => {
+    console.log(formRef.current.getFieldsValue());
+    debugger;
+  };
   return (
-    <Space style={{ padding: 10 }}>
-      <Button type="primary">登录</Button>
-    </Space>
+    <Col span={12} push={3}>
+      <Form ref={formRef}>
+        <Row align="middle" gutter={[0, 12]}>
+          <Col span={24}>
+            <Form.Item label="账号" name="useName">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item label="密码" name="passWord">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Space>
+              <Button type="primary" onClick={handleLogin}>
+                登录
+              </Button>
+            </Space>
+          </Col>
+        </Row>
+      </Form>
+    </Col>
   );
 };
 

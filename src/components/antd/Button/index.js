b6f03b27280b7065219f4b9a88334f2827b6e996
tzd <tzd>
2022-08-13 10:58:07 +0800

    二次封装antd

---
+++ src/components/antd/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Button } from 'antd'
+
+const TButton = ({ ...others }) => {
+  return <Button {...others} />
+}
+
+export default TButton
---
+++ src/components/antd/index.js
@@ -0,0 +1,10 @@
+import Spin from './Spin'
+import Button from './Button'
+
+Object.defineProperty(exports, "Button", {
+    value: Button,
+})
+
+Object.defineProperty(exports, "Spin", {
+    value: Spin,
+})

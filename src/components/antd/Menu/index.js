e6dc954f8d4f9a811c83f192341af59fc9f28d21
tzd <tzd>
2022-08-27 00:30:01 +0800

    修改结构

--- src/components/antd/Menu/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Menu } from 'antd'
-
-const TMenu = ({ ...other }) => {
-  return <Menu {...other} />
-}
-
-export default TMenu
--- src/components/antd/Spin/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Spin } from 'antd'
-
-const TSpin = ({ ...other }) => {
-  return <Spin {...other} />
-}
-
-export default TSpin

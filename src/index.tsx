a74d462c9ddad40fb4be26c5d31d081704928242
tianzhengdong <582361831@qq.com>
2023-06-17 20:33:12 +0800

    删除无用代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React, { Fragment } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import Bubble from "./components/Bubble";
 
 const root = document.getElementById("root");
+
 if (root) {
   createRoot(root).render(
     <Fragment>

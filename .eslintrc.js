8c016dafb4a3759307d2167db2f694b9d5449f54
tianzhengdong <582361831@qq.com>
2023-08-30 11:16:42 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,10 @@ module.exports = {
   // extends: ["eslint:recommended"], // 这一行被注释掉了，意味着没有继承特定的扩展配置
   plugins: ["@typescript-eslint", "react", "react-hooks", "import"],
   rules: {
-    'no-debugger': 'warn',
+    'no-debugger': 'warn', // 有debugger警告提示
     "@typescript-eslint/explicit-module-boundary-types": "error",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off", // @typescript-eslint/no-var-requires 是 TypeScript ESLint 插件提供的一条规则，用于在 TypeScript 代码中禁止使用 require 语句来导入模块，而鼓励使用 ES6 模块的 import 语句。
     "react/jsx-uses-react": "error", // 强制在 JSX 使用 React
     "react/jsx-uses-vars": "error", // 检测未使用的 JSX 变量
     "no-undef": "error", // 对未定义（未知变量）但是使用了的做规则
@@ -33,8 +34,13 @@ module.exports = {
     "react/react-in-jsx-scope": "error", // 强制在 JSX 中使用 React
     "react-hooks/rules-of-hooks": "error", // 强制遵循 React Hooks 使用规则
     "react-hooks/exhaustive-deps": "warn", // 检测依赖项数组是否完整
-    "import/prefer-default-export": "off",
-    "import/no-named-as-default": "off"
+    "import/prefer-default-export": "off", // 用于控制在模块中是使用默认导出（default export）而不是命名导出（named export）
+    'import/first': 'error', // 规则要求将所有的导入语句放在文件的顶部
+    "import/no-anonymous-default-export": "error", // error 不允许默认匿名导出
+    "import/no-extraneous-dependencies": "error",
+    // "import/no-unresolved": ["error", { "commonjs": true }]
+    // "import/no-named-as-default": "off", // 设置为 "off，允许将命名导出作为默认导出来导入
+    // "import/no-unresolved": ["error", { "commonjs": true }]
     // 'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }]
     // 上面这行被注释掉了，如果启用会检查未解析的导入模块
   },

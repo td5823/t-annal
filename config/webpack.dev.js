7b2f34c6d9e2809dfe5e7ec664cddaf22cc06c0c
tianzhengdong <582361831@qq.com>
2023-09-02 12:19:14 +0800

    feat:  禁用覆盖层 在浏览器中以百分比显示编译进度

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -11,10 +11,19 @@ const paths = require("./paths.js");
 module.exports = merge(baseConfig, {
   mode: "development", // 开发模式,打包更加快速,省了代码优化步骤
   devtool: "eval-cheap-module-source-map", // 源码调试模式,后面会讲
+  // performance: {
+  //   // 控制是否显示性能提示
+  //   hints: 'warning', // 将警告级别设置为 'warning'
+  // },
   devServer: {
     port: 1000, // 服务端口号
     compress: false, // gzip压缩,开发环境不开启,提升热更新速度
-    hot: true, // 开启热更新，后面会讲react模块热替换具体配置
+    hot: true, // 开启热更新
+    client: {
+      overlay: false,  // 禁用覆盖层
+      progress: true, // 在浏览器中以百分比显示编译进度
+    },
+    // progress: true, // 启用进度显示
     historyApiFallback: true, // 解决history路由404问题
     setupMiddlewares: (middlewares, devServer) => {
       if (!devServer) {
@@ -39,10 +48,13 @@ module.exports = merge(baseConfig, {
       directory: path.join(__dirname, "../public"), //托管静态资源public文件夹
     },
   },
+  // stats: 'errors-warnings',
   plugins: [
     // ...其它插件...
     new ESLintPlugin({
       extensions: ["js", "jsx", "ts", "tsx"],
+      failOnError: true, // 确保不会因 ESLint 警告而中断构建
+      overrideConfigFile: path.resolve(`${__dirname}/..`, '.eslintrc.js'), // 指定 ESLint 配置文件的路径
     }),
   ],
 });
